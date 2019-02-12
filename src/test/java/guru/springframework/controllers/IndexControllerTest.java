053662aa186abc6f110a07f705e680a6ccc9fa5d
Bondar Ferenc <fhery021@gmail.com>
2019-02-12 16:29:28 +0200

    Adding bootstrap, unit test for IndexController, Category, RecipeService

---
+++ src/test/java/guru/springframework/controllers/IndexControllerTest.java
@@ -0,0 +1,44 @@
+package guru.springframework.controllers;
+
+import guru.springframework.services.RecipeService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.anySet;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+/**
+ * Created by Ferenc on 2/12/2019.
+ */
+public class IndexControllerTest {
+
+    IndexController indexController;
+
+    @Mock
+    RecipeService recipeService;
+
+    @Mock
+    Model model;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        indexController = new IndexController(recipeService);
+    }
+
+    @Test
+    public void getIndexPage() throws Exception {
+        assertEquals("index", indexController.getIndexPage(model));
+
+        verify(recipeService, times(1)).getRecipes();
+
+        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+    }
+
+}
